ba9d26a609326b1d5cc700d1f7465feefc128f35
NikolasMosca <ciokkkolate@yahoo.it>
2022-07-22 17:03:55 +0200

    small fix related to build for production

--- src/use/stats.js
+++ src/use/stats.js
@@ -1,15 +1,15 @@
 import { ref } from "vue";
 
 export default function useStats() {
-    return ref((() => {
-        try {
-          return JSON.parse(localStorage.getItem('stats'))
-        } catch(e) {
-          return {
-            player: 0,
-            table: 0,
-            draw: 0
-          }
-        }
-    })())
+    let initialState = {
+        player: 0,
+        table: 0,
+        draw: 0
+    }
+    try {
+        initialState = JSON.parse(localStorage.getItem('stats'))
+    } catch(e) {
+        console.log(e)
+    }
+    return ref(initialState)
 }
